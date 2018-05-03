43cc15a410cb8d626bd7f0a1e4096897d698b0df
Cody Melton <camelto2@ncsu.edu>
2018-05-03 16:54:21 -0400

    fixed BFD in Sc

--- spectra/calc_mads.py
+++ spectra/calc_mads.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('Mn_new.dat',delimiter='&',index_col=0)
+df = pd.read_csv('Sc.dat',delimiter='&',index_col=0)
 
 print(df)
 print(df.iloc[:5].abs().mean().round(6).tolist())
