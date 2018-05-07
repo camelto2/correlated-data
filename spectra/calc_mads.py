4ec393914e5f55915c64820746f1048e30bcc1f5
Cody Melton <camelto2@ncsu.edu>
2018-05-07 12:27:52 -0400

    updated spectra

--- spectra/calc_mads.py
+++ spectra/calc_mads.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import sys
 
-df = pd.read_csv('Mn.dat',delimiter='&',index_col=0)
+
+df = pd.read_csv('Co_new.dat',delimiter='&',index_col=0)
 
 print(df)
 print(df.iloc[:5].abs().mean().round(6).tolist())
