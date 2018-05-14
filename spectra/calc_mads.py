1c5d150361ca1ed723107e70e5ece19ceb88c969
Cody Melton <camelto2@ncsu.edu>
2018-05-14 18:34:40 -0400

    New NiH and Ni data

--- spectra/calc_mads.py
+++ spectra/calc_mads.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 
 
-df = pd.read_csv('Co_new.dat',delimiter='&',index_col=0)
+df = pd.read_csv('Ni.dat',delimiter='&',index_col=0)
 
 print(df)
 print(df.iloc[:5].abs().mean().round(6).tolist())
